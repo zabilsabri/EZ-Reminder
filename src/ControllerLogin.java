8c518a52da0e5bbb92d3a988224b8ebe053a9908
zabil sabri muhammad <zabilsabrimuhammad0@gmail.com>
2022-06-06 07:19:33 +0800

    multi scene

---
+++ src/ControllerLogin.java
@@ -0,0 +1,24 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ControllerLogin {
+
+    @FXML
+    private PasswordField passWordF;
+
+    @FXML
+    private TextField userNameF;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUp(ActionEvent event) {
+
+    }
+
+}
